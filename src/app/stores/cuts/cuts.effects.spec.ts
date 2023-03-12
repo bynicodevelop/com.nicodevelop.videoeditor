fd3cf518058055e27e47ee885a14fb52091576cb
nico-develop <contact@nico-develop.fr>
2023-03-12 09:35:53 +0100

    common - Optimisation des tests sur les cuts

--- src/app/stores/cuts/cuts.effects.spec.ts
+++ src/app/stores/cuts/cuts.effects.spec.ts
@@ -1,7 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
 
+import { Observable } from 'rxjs';
+import { RegionEntity } from 'src/app/models/region';
+
+import { Actions } from '@ngrx/effects';
+import { provideMockActions } from '@ngrx/effects/testing';
+
+import { addCuts, convertRegion } from './cuts.actions';
 import { CutsEffects } from './cuts.effects';
 
 describe('CutsEffects', () => {
@@ -9,11 +14,13 @@ describe('CutsEffects', () => {
   let effects: CutsEffects;
 
   beforeEach(() => {
+    const actionsMock = jasmine.createSpyObj('Actions', ['pipe']);
     TestBed.configureTestingModule({
       providers: [
         CutsEffects,
-        provideMockActions(() => actions$)
-      ]
+        { provide: Actions, useValue: actionsMock },
+        provideMockActions(() => actions$),
+      ],
     });
 
     effects = TestBed.inject(CutsEffects);
@@ -21,5 +28,116 @@ describe('CutsEffects', () => {
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  it('Should return empty array of cuts (1)', () => {
+    const regions = [
+      {
+        uid: 'region-1',
+        start: 1,
+        end: 3,
+        duration: 2,
+      } as RegionEntity,
+    ];
+
+    const duration = 10;
+
+    actions$ = new Observable((observer) => {
+      observer.next(convertRegion({ regions, duration }));
+    });
+
+    effects.addRegion$.subscribe((action) => {
+      console.log(action);
+
+      expect(action).toEqual(
+        addCuts({
+          cuts: [
+            { uid: 'cut-1', start: 0, end: 1 },
+            { uid: 'cut-2', start: 3, end: 10 },
+          ],
+        })
+      );
+    });
+  });
+
+  it('Should return empty array of cuts (2)', () => {
+    const regions = [
+      {
+        uid: 'region-1',
+        start: 0,
+        end: 3,
+        duration: 2,
+      } as RegionEntity,
+    ];
+
+    const duration = 10;
+
+    actions$ = new Observable((observer) => {
+      observer.next(convertRegion({ regions, duration }));
+    });
+
+    effects.addRegion$.subscribe((action) => {
+      console.log(action);
+
+      expect(action).toEqual(
+        addCuts({
+          cuts: [{ uid: 'cut-1', start: 3, end: 10 }],
+        })
+      );
+    });
+  });
+
+  it('Should return empty array of cuts (3)', () => {
+    const regions = [
+      {
+        uid: 'region-1',
+        start: 8,
+        end: 10,
+        duration: 2,
+      } as RegionEntity,
+    ];
+
+    const duration = 10;
+
+    actions$ = new Observable((observer) => {
+      observer.next(convertRegion({ regions, duration }));
+    });
+
+    effects.addRegion$.subscribe((action) => {
+      console.log(action);
+
+      expect(action).toEqual(
+        addCuts({
+          cuts: [{ uid: 'cut-1', start: 0, end: 8 }],
+        })
+      );
+    });
+  });
+
+  it('Should return empty array of cuts (4)', () => {
+    const regions = [
+      {
+        uid: 'region-1',
+        start: 0.2,
+        end: 3,
+        duration: 2,
+      } as RegionEntity,
+    ];
+
+    const duration = 10;
+
+    actions$ = new Observable((observer) => {
+      observer.next(convertRegion({ regions, duration }));
+    });
+
+    effects.addRegion$.subscribe((action) => {
+      console.log(action);
+
+      expect(action).toEqual(
+        addCuts({
+          cuts: [{ uid: 'cut-1', start: 3, end: 10 }],
+        })
+      );
+    });
   });
 });
--- src/app/stores/cuts/cuts.effects.ts
+++ src/app/stores/cuts/cuts.effects.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 
-import { filter, map } from 'rxjs';
+import { filter, map, tap } from 'rxjs';
 import { CutEntity } from 'src/app/models/cuts';
 
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 
 import { addCuts, convertRegion } from './cuts.actions';
+
+const MIN_CUT_DURATION = 0.3;
 
 @Injectable()
 export class CutsEffects {
@@ -14,17 +16,21 @@ export class CutsEffects {
   addRegion$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(convertRegion),
-      filter(({ regions }): boolean => {
-        const result = regions.filter(
-          (region): boolean => region.start !== 0 && region.end !== 0
-        );
-
-        return result.length === regions.length;
+      filter(({ regions }): boolean =>
+        regions.every((region) => region.start !== 0 && region.end !== 0)
+      ),
+      map(({ regions, duration }) => {
+        if (regions.length === 1 && regions[0].start < MIN_CUT_DURATION) {
+          return {
+            regions: [{ ...regions[0], start: 0 }],
+            duration,
+          };
+        }
+        return { regions, duration };
       }),
       map(({ regions, duration }): CutEntity[] => {
         const cuts: CutEntity[] = [];
 
-        // Add a "silent" region if the first region doesn't start at 0
         if (regions[0].start > 0) {
           cuts.push({
             uid: `cut-${cuts.length + 1}`,
@@ -33,20 +39,21 @@ export class CutsEffects {
           });
         }
 
-        // Iterate over the regions and create cuts as needed
         for (let i = 0; i < regions.length; i++) {
           const region = regions[i];
           const nextRegion = regions[i + 1];
 
+          if (region.start < MIN_CUT_DURATION) {
+            region.start = region.end;
+          }
+
           if (nextRegion && region.end < nextRegion.start) {
-            // Create a cut between the current region and the next one
             cuts.push({
               uid: `cut-${cuts.length + 1}`,
               start: region.end,
               end: nextRegion.start,
             });
           } else if (!nextRegion && region.end < duration) {
-            // Create a cut between the last region and the end of the track
             cuts.push({
               uid: `cut-${cuts.length + 1}`,
               start: region.end,
@@ -57,6 +64,7 @@ export class CutsEffects {
 
         return cuts;
       }),
+      tap(console.log),
       map((cuts) => addCuts({ cuts }))
     );
   });
